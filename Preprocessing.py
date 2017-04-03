8963169ca5a08209cb71b0a2cf4bb6e273905346
binghust <jianghuqixialb@sina.com>
2017-04-03 22:04:02 +0800

    1. Capitalize the first letter in each note. 2. Splie data by user. 3. Fix a bug about engline characteristic \r\n.

--- Preprocessing.py
+++ Preprocessing.py
@@ -9,12 +9,13 @@ def reorder_array(source_array):
 # implemented in numpy.loadtxt, the first 5 columns in the new dataset contains b'', need to be deleted manually
 # could also be implemented in pandas.read_csv
 def reformat_dataset(source_path, target_path):
-    #  open file
+    #  Open file
     with open(source_path, 'r') as source_fid:
-        # load the original dataset to a row_num x 6 list
-        data = [row.rstrip('\r\n').split('\t') for row in source_fid]
+        # \r, \n and \r\n will be substituted by \n while reading file if the parameter newline is not specified
+        # Load the original dataset to a row_num x 6 list
+        data = [row.rstrip('\n').split('\t') for row in source_fid]
 
-        # delete rows including invalid coordinate such as lat/lon==0, lat<-90/>90, or lon<-180/>180
+        # Delete rows including invalid coordinate such as lat/lon==0, lat<-90/>90, or lon<-180/>180
         invalid_row_index = []
         for row_index in range(len(data)-1, -1, -1):
             latitude = float(data[row_index][3])
@@ -25,32 +26,59 @@ def reformat_dataset(source_path, target_path):
                 del data[row_index]
         print('Invalid row(s):', invalid_row_index)
 
-        # reverse six columns to keep chronological order
+        # Reverse six columns to keep chronological order
         data = data[::-1]
 
-        # reorder the 5th column loc_id as a int list loc_ids_reordered
+        # Reorder the 5th column loc_id as a int list loc_ids_reordered
         loc_ids_raw = [row[5] for row in data]
         loc_ids_reordered = reorder_array(loc_ids_raw)
 
-        # write to a new dataset
+        # Write to a new dataset
         delimiter = '\t'
         with open(target_path, 'w') as target_fid:
             for i in range(0, len(data)):
-                row = delimiter.join(data[i][0:5]) + delimiter + str(loc_ids_reordered[i]) + '\r\n'
+                # \n will be substituted by \r, \n or \r\n  while writing file according to your operation system
+                # If the parameter newline is not specified
+                row = delimiter.join(data[i][0:5]) + delimiter + str(loc_ids_reordered[i]) + '\n'
+                # If ended in '\r\n', it will be substituted by '\r\r\n'
                 target_fid.write(row)
+
+
+def split_by_user(source_path):
+    #  Open file
+    with open(source_path, 'r') as source_fid:
+        data_by_user = []
+        data_of_one_user = ''
+        user_id_previous = ''
+        for row in source_fid:
+            user_id = row.rstrip('\n').split('\t')[0]
+            if user_id != user_id_previous:
+                data_by_user.append(data_of_one_user)
+                data_of_one_user = row
+            else:
+                data_of_one_user += row
+            user_id_previous = user_id
+        # Delete the first element in data_by_user since it is ''.
+        del data_by_user[0]
+        # Append data of the last user to data_by_user
+        data_by_user.append(data_of_one_user)
+
 
 # reformat_dataset(
 #     r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Gowalla\checkins_preview.txt',
 #     r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Gowalla\checkins_preview_valid_reordered.txt')
-#
+
 # reformat_dataset(
 #     r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Brightkite\checkins_preview.txt',
 #     r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Brightkite\checkins_preview_valid_reordered.txt')
-#
+
 # reformat_dataset(
 #     r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Gowalla\checkins.txt',
 #     r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Gowalla\checkins_valid_reordered.txt')
 
-reformat_dataset(
-    r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Brightkite\checkins.txt',
-    r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Brightkite\checkins_valid_reordered.txt')
+# reformat_dataset(
+#     r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Brightkite\checkins.txt',
+#     r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Brightkite\checkins_valid_reordered.txt')
+
+split_by_user(
+    r'D:\Workspace\Datasets\Location-Based Social Network\SNAP Brightkite\checkins_preview_valid_reordered.txt')
